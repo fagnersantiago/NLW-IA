a43c271cd1110e6eaf930b7f09cab41bb7034e8e
Fagner Santiago <fagner.santiago@hotmail.com>
2022-03-17 17:47:02 -0300

    testando rota post

--- fireBaseWithTs/functions/src/index.ts
+++ fireBaseWithTs/functions/src/index.ts
@@ -1,8 +1,17 @@
+import * as firebase_admin from "firebase-admin";
 import * as functions from "firebase-functions";
+import * as express from "express";
 
-// Start writing Firebase Functions
-// https://firebase.google.com/docs/functions/typescript
+firebase_admin.initializeApp(functions.config().firebase);
 
-export const helloWorld = functions.https.onRequest((request, response) => {
-  response.send("Hello from Firebase!");
-});
+const app = express();
+const main = express();
+
+main.use("/api/v1", app);
+main.use(express.json());
+
+export const db = firebase_admin.firestore();
+export const userColection = "users";
+export const firstApiWithFirebase = functions.https.onRequest(main);
+
+export default app;
