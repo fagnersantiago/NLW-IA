4fd89d2a3c40f749991d8aa914437355fdf105ca
Fagner Santiago <fagner.santiago@hotmail.com>
2023-09-12 14:46:34 -0300

    fix: fixing component textarea and component select

--- upload-ia-web/src/App.tsx
+++ upload-ia-web/src/App.tsx
@@ -9,8 +9,9 @@ import {
   SelectItem,
   SelectTrigger,
   SelectValue,
-} from "@radix-ui/react-select";
-import { Slider } from "@radix-ui/react-slider";
+} from "./components/ui/select";
+
+import { Slider } from "./components/ui/slider";
 
 export function App() {
   return (
@@ -30,8 +31,8 @@ export function App() {
       </div>
 
       <main className="flex-1 p-6 flex gap-6">
-        <div className="flex flex-col gap-4">
-          <div className="grid grid-row-2  gap-4 flex-2">
+        <div className="flex flex-col flex-1 gap-4">
+          <div className="grid grid-rows-2 gap-4 flex-1">
             <Textarea
               className="resize-none p-4 leading-relaxed"
               placeholder="Inclua o prompt para IA..."
@@ -47,73 +48,73 @@ export function App() {
             <code className="text-violet-400">{"{transcription}"}</code> no seu
             prompt para adicionar o conteudo da trascrição do video selecionado.
           </p>
-          <aside className="w-80 space-y-6">
-            <form className="space-y-6">
-              <label
-                htmlFor="video"
-                className="border flex rounded-md aspect-video cursor-point border-dashed text-sm flex-col gep-2  items-center justify-center text-muted-foreground houver:bg-primary/5"
-              >
-                <FileVideo className="w-4 h-4" />
-                Selecione um vídeo
-              </label>
-
-              <input
-                type="file"
-                id="video"
-                accept="video/mp4"
-                className="sr-only"
-              />
-              <Separator />
-              <div className="space-y-2">
-                <Label htmlFor="transciption-prompt">Prompt de transição</Label>
-
-                <Textarea
-                  id="transciption-prompt"
-                  className="h-20 leading-relaxed resize-none"
-                  placeholder="Inclua palavras-chave mencionada no vído separadas por vírgula (,)"
-                />
-              </div>
-
-              <Button className="w-full">
-                Carregar vídeo
-                <Upload className="m-4-h-4 ml-2" />
-              </Button>
-            </form>
-            <form className="space-y-6">
-              <div className="space-y-2">
-                <Label>Modelo</Label>
-                <Select disabled defaultValue="GPT3.5">
-                  <SelectTrigger>
-                    <SelectValue />
-                  </SelectTrigger>
-                  <SelectContent>
-                    <SelectItem value="GPT3.5">GPT3.5</SelectItem>
-                  </SelectContent>
-                </Select>
-
-                <span className="block">
-                  você poderá customizar essa opção em breve
-                </span>
-              </div>
-
-              <Separator />
-              <div className="space-y-4">
-                <Label>Temperatura</Label>
-                <Slider min={0} max={1} step={0.1} />
-
-                <span className="block text-xs text-muted-foreground italic">
-                  Valores mais altos podem deixar o resultado mais criativos e
-                  com possíveis erros
-                </span>
-              </div>
-              <Separator />
-              <Button className="w-full" type="submit">
-                Executar
-                <Wand2 className="w-4 h-4 ml-2" />
-              </Button>
-            </form>
-          </aside>
         </div>
+
+        <aside className="w-80 space-y-6">
+          <form className="space-y-6">
+            <label
+              htmlFor="video"
+              className="border flex rounded-md aspect-video cursor-point border-dashed text-sm flex-col gep-2  items-center justify-center text-muted-foreground houver:bg-primary/5"
+            >
+              <FileVideo className="w-4 h-4" />
+              Selecione um vídeo
+            </label>
+
+            <input
+              type="file"
+              id="video"
+              accept="video/mp4"
+              className="sr-only"
+            />
+            <Separator />
+            <div className="space-y-2">
+              <Label htmlFor="transciption-prompt">Prompt de transição</Label>
+
+              <Textarea
+                id="transciption-prompt"
+                className="h-20 leading-relaxed resize-none"
+                placeholder="Inclua palavras-chave mencionada no vído separadas por vírgula (,)"
+              />
+            </div>
+
+            <Button className="w-full">
+              Carregar vídeo
+              <Upload className="m-4-h-4 ml-2" />
+            </Button>
+          </form>
+          <form className="space-y-6">
+            <div className="space-y-2">
+              <Label>Prompt</Label>
+              <Select>
+                <SelectTrigger>
+                  <SelectValue placeholder="Selecione um prompt..." />
+                </SelectTrigger>
+                <SelectContent>
+                  <SelectItem value="title ">Título do Youtube</SelectItem>
+                  <SelectItem value="description">
+                    Descrição do Youtube
+                  </SelectItem>
+                </SelectContent>
+              </Select>
+            </div>
+
+            <Separator />
+            <div className="space-y-4">
+              <Label>Temperatura</Label>
+              <Slider min={0} max={1} step={0.1} />
+
+              <span className="block text-xs text-muted-foreground italic">
+                Valores mais altos podem deixar o resultado mais criativos e com
+                possíveis erros
+              </span>
+            </div>
+            <Separator />
+            <Button className="w-full" type="submit">
+              Executar
+              <Wand2 className="w-4 h-4 ml-2" />
+            </Button>
+          </form>
+        </aside>
       </main>
     </div>
   );
