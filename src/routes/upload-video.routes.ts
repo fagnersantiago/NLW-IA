d357b45c0079e10b5e5b2ced76dc4b9a4bedacbe
Fagner Santiago <fagner.santiago@hotmail.com>
2023-09-13 11:36:40 -0300

    feat creating back-end like routes get all prompts and upload video

---
+++ src/routes/upload-video.routes.ts
@@ -0,0 +1,42 @@
+import { FastifyInstance } from "fastify";
+import { fastifyMultipart } from "@fastify/multipart";
+import path from "node:path";
+import { randomUUID } from "node:crypto";
+import { pipeline } from "node:stream";
+import { promisify } from "node:util";
+import fs from "fs";
+
+const pump = promisify(pipeline);
+
+export async function uploadVideo(app: FastifyInstance) {
+  app.register(fastifyMultipart, {
+    limits: {
+      fieldSize: 1048576 * 25,
+    },
+  });
+
+  app.post("/video", async (request, reply) => {
+    const data = await request.file();
+
+    if (!data) {
+      return reply.status(400).send({ error: " Missing file input" });
+    }
+
+    const extension = path.extname(data.fieldname);
+
+    if (extension !== "mp3") {
+      return reply
+        .status(400)
+        .send({ error: "Invalid format, please upload a mp3" });
+    }
+
+    const filebaseName = path.basename(data.fieldname, extension);
+    const fileUpload = `${filebaseName}-${randomUUID()}-${extension}}`;
+
+    const uloadDestination = path.resolve(__dirname, "../../tmp", fileUpload);
+
+    await pump(data.file, fs.createWriteStream(uloadDestination));
+
+    return reply.send();
+  });
+}
